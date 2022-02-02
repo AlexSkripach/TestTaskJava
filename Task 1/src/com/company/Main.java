200b297254cd8270a61ab986869ce42051707708
AlexSkripach <98901611+AlexSkripach@users.noreply.github.com>
2022-02-03 00:34:05 +0300

    Version 1.2 has been aded

--- Task 1/src/com/company/Main.java
+++ Task 1/src/com/company/Main.java
@@ -7,26 +7,32 @@ public class Main
 
     public static void main(String[] args)throws java.io.IOException
     {
-        String number;
+        String number1, number2;
         double result;
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter your number: ");
-        number = in.next();
-        if(Check(number) == true) {
-            result = Double.parseDouble(number);
+        System.out.println("Enter your first number: ");
+        number1 = in.next();
+        System.out.println("Enter your second number: ");
+        number2 = in.next();
+        Check2(number1);
+        Check2(number2);
+    }
+
+    public static void Check2(String number)
+    {
+        if (Check1(number) == true) {
+            double result = Double.parseDouble(number);
             if (result % 2 == 0) {
                 System.out.println("Even number\n");
             } else {
                 System.out.println("Not even number\n");
             }
-        }
-        else
-        {
+        } else {
             System.out.println("Input is not a digit\n");
         }
     }
 
-    public static boolean Check(String number)
+    public static boolean Check1(String number)
     {
         int tmp = 0;
         char[] Numbers = number.toCharArray();
