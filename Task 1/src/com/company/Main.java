f1edffff3baca27a86f75d33e21504176e35c9d2
AlexSkripach <98901611+AlexSkripach@users.noreply.github.com>
2022-02-03 00:05:13 +0300

    Version 1.1 has been added

--- Task 1/src/com/company/Main.java
+++ Task 1/src/com/company/Main.java
@@ -4,20 +4,47 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String[] args)
+
+    public static void main(String[] args)throws java.io.IOException
     {
-        int number;
+        String number;
+        double result;
         Scanner in = new Scanner(System.in);
         System.out.println("Enter your number: ");
-        number = in.nextInt();
-        if (number % 2 == 0)
-        {
-            System.out.println("Even number\n");
+        number = in.next();
+        if(Check(number) == true) {
+            result = Double.parseDouble(number);
+            if (result % 2 == 0) {
+                System.out.println("Even number\n");
+            } else {
+                System.out.println("Not even number\n");
+            }
         }
         else
         {
-            System.out.println("Not even number\n");
+            System.out.println("Input is not a digit\n");
         }
+    }
 
+    public static boolean Check(String number)
+    {
+        int tmp = 0;
+        char[] Numbers = number.toCharArray();
+
+        for(int i = 0; i < Numbers.length; i++) {
+
+            if (Character.isDigit(Numbers[i]) | Numbers[i]=='.') {
+            }
+            else {
+                tmp++;
+            }
+        }
+        if(tmp == 0) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
