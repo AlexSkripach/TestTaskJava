85d15d5c52ee88b8d3dca74b2cc729d93e906938
AlexSkripach <98901611+AlexSkripach@users.noreply.github.com>
2022-02-04 16:34:19 +0300

    Done 1.1

--- 2 Task Java/src/com/company/Main.java
+++ 2 Task Java/src/com/company/Main.java
@@ -3,10 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int[] Array = new int[100];
+        int tmp=0;
+        String [] Array = new String[100];
         for(int i=0; i < 100; i++ )
         {
-            Array[i] = i+1;
+            Array[i] = (tmp+1) + "";
+            Array[i+1] = "&";
+            tmp++;
+            i++;
+        }
+        for (int i = 0; i < 100; i++)
+        {
             System.out.println("Element " + (i+1) + ": " + Array[i]);
         }
     }
