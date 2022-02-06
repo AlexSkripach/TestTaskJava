ed4672bd73a99847fd4a3905aecad1d48ffbb0ce
AlexSkripach <98901611+AlexSkripach@users.noreply.github.com>
2022-02-06 23:21:06 +0300

    Done 1.3

--- 2 Task Java/src/com/company/Main.java
+++ 2 Task Java/src/com/company/Main.java
@@ -1,28 +1,36 @@
 package com.company;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int tmp=0;
-        String [] Array1 = new String[100];
-
-        for(int i=0; i < 100; i++ )
+    public static void main(String[] args)
+    {
+        int n, tmp=1;
+        System.out.println("Enter how many items Array will take: ");
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        String[] Array1 = new String[n];
+        for(int i = 0; i < n; i++)
         {
-            if((tmp+1) % 2 == 0) {
-                Array1[i] = (tmp + 1) + "";
-                Array1[i + 1] = "&";
-                tmp++;
-                i++;
-            }
-            else
+            for(int j = tmp; j != 100; j++)
             {
-                tmp++;
-                i--;
+                if(j%2 == 0)
+                {
+                    Array1[i] = j +"";
+                    tmp = j+1;
+                    j = 99;
+                    if(i != (n-1))
+                    {
+                        Array1[i+1] = "&";
+                        i++;
+                    }
+                }
             }
         }
-        for (int i = 0; i < 100; i++)
-        {
-            System.out.println("Element " + (i+1) + ": " + Array1[i]);
+        for (int i = 0; i < n; i++)
+            {
+                System.out.println("Element " + (i + 1) + ": " + Array1[i]);
+            }
         }
     }
-}
